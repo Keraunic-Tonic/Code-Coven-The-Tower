f542f1e80b077c48dce9054d4998ba92fdf748f5
Dian Delph <da.dnvl@live.com>
2020-07-23 19:44:57 -0500

    Adds PlasmaSaw rotation, movement (x and y axis) Adds Laser shooting

---
+++ The Tower/Assets/Scenes/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+	public float speed = 5f;
+	Rigidbody2D rb;
+
+	private void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		rb.AddForce(transform.right * speed, ForceMode2D.Impulse);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "worldLimits")
+		{
+			Destroy(gameObject);
+		}
+
+	}
+}
---
+++ The Tower/Assets/Scenes/Shooting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+	public Rigidbody2D laser;
+	public GameObject soundManager;
+	public bool isVertical;
+	public float firingRate;
+	void Start()
+	{
+
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		if (isVertical)
+		{
+			Invoke("VerticalFiring", firingRate);
+		}
+		else
+		{
+			Invoke("HorizontalFiring", firingRate);
+		}
+	}
+
+	void VerticalFiring()
+	{
+		Instantiate(laser, gameObject.transform.position, Quaternion.Euler(new Vector3(0, 0, 90)));
+	}
+	void HorizontalFiring()
+	{
+		Instantiate(laser, gameObject.transform.position, Quaternion.Euler(new Vector3(0, 0, 90)));
+	}
+}
+
+
+
+
