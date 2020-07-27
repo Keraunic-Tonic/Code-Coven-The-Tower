b1a6b65fa67032da43e6b39e421216320da6cf60
Dian Delph <da.dnvl@live.com>
2020-07-27 18:48:29 -0500

    Adds cannon shooting script, laser script.

--- The Tower/Assets/Scenes/LaserBehaviour.cs
+++ The Tower/Assets/Scenes/LaserBehaviour.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class LaserBehaviour : MonoBehaviour
 {
+    public float speed = 5f;
+    Rigidbody2D rb;
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        myAudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		rb.AddForce(transform.right * speed, ForceMode2D.Impulse);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "worldLimits")
+		{
+			Destroy(gameObject);
+		}
+	}
 }
